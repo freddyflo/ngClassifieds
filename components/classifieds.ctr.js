25110ab324f721d0625aa700426340b709a70743
freddyflo <fred.aklamanu@gmail.com>
2017-05-25 11:05:57 +0200

    Create + Update Implemented

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -3,7 +3,7 @@
     
     angular
     .module("ngClassifieds")
-    .controller("classifiedCtrl", function($scope, $http, classifiedsFactory, $mdSidenav) {
+    .controller("classifiedCtrl", function($scope, $http, classifiedsFactory, $mdSidenav, $mdToast) {
         
      classifiedsFactory.getClassifieds().then(function(classified) {
           $scope.classifieds = classified.data;
@@ -17,5 +17,40 @@
        $scope.closeSideBar = function() {
           $mdSidenav('left').close();
       }
+       
+       $scope.saveClassified = function(classified){
+           if(classified) { 
+            $scope.classifieds.push(classified);
+            $scope.classified = {};
+            $scope.closeSideBar();
+            showToast("Classified Saved");
+        
+       }
+    }
+       
+       // edit classified
+       $scope.editClassified = function(classified) {
+           $scope.editing = true;
+           $scope.openSideBar();
+           $scope.classified = classified;
+       }
+       
+       // save edit
+        $scope.saveEdit = function() {
+            $scope.editing = false;
+            $scope.classified = {};
+            $scope.closeSideBar();
+            showToast("Edit Saved");
+        }
+        
+        // TOAST 
+        function showToast(message) {
+                $mdToast.show(
+                $mdToast.simple()
+                .content(message)
+                .position("top, right")
+                .hideDelay(3000)
+           );
+        }
  });
 })(); 
