2f823c3c47a7e7a369bf35d3f3c9312b31f11aec
freddyflo <fred.aklamanu@gmail.com>
2017-06-01 22:16:53 +0200

    CRUD done

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -3,13 +3,16 @@
     
     angular
     .module("ngClassifieds")
-    .controller("classifiedCtrl", function($scope, $http, classifiedsFactory, $mdSidenav, $mdToast) {
+    .controller("classifiedCtrl", function($scope, $http, classifiedsFactory, $mdSidenav, $mdToast, $mdDialog) {
         
      classifiedsFactory.getClassifieds().then(function(classified) {
           $scope.classifieds = classified.data;
+          $scope.categories = getCategories($scope.classifieds);
          //  console.log(classified);
       });
         
+
+    
       $scope.openSideBar = function() {
           $mdSidenav('left').open();
       }
@@ -43,6 +46,24 @@
             showToast("Edit Saved");
         }
         
+      
+        
+        // delete
+        $scope.deleteClassified = function(event, classified) {
+            
+            var confirm = $mdDialog.confirm()
+                .title('Are you sure you want to delete ' + classified.title + '?')
+                .ok('Yes')
+                .cancel('No')
+                .targetEvent(event);
+            $mdDialog.show(confirm).then(function() {
+                 var index = $scope.classifieds.indexOf(classified); 
+                 $scope.classifieds.splice(index,1);
+            }, function(){
+                
+            });    
+        }
+        
         // TOAST 
         function showToast(message) {
                 $mdToast.show(
@@ -51,6 +72,19 @@
                 .position("top, right")
                 .hideDelay(3000)
            );
+        }
+        
+        // aggegration
+        function getCategories(classifieds) {
+            
+            var categories = [];
+            
+            angular.forEach(classifieds, function(item){
+                angular.forEach(item.categories, function(category){
+                    categories.push(category);
+                });
+            });
+                return _.uniq(categories);
         }
  });
 })(); 
